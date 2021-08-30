edbbb5137b8d85cfc981b868da2cf0ceb6c5457f
Lou Rosas <lourosas@gmail.com>
2021-08-30 09:12:23 -0700

    08-30-21-00

---
+++ rosas/lou/clock/LTimerHelpJDialog.java
@@ -0,0 +1,174 @@
+//////////////////////////////////////////////////////////////////////
+/*
+The LTimer Application.
+Copyright (C) 2021 Lou Rosas
+This file is part of the LTimer
+LTimter is free software; you can redistribute it
+and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+LTimer is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+//////////////////////////////////////////////////////////////////////
+package rosas.lou.clock;
+
+import java.lang.*;
+import java.util.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.text.*;
+
+//////////////////////////////////////////////////////////////////////
+package rosas.lou.clock;
+
+public class LTimerHelpJDialog extends JDialog{
+   private static final short WIDTH  = 350;
+   private static final short HEIGHT = 600;
+
+   private static LTimerHelpJDialog _instance;
+   private String _helpText;
+
+   {
+      _instance = null;
+      _helpText = null;
+   }
+
+   ////////////////////Public Methods/////////////////////////////////
+   /**/
+   public static LTimerHelpJDialog instance(JFrame frame, String txt){
+      if(_instance == null){
+         _instance = new LTimerHelpJDialog(frame, txt);
+      }
+      return instance;
+   }
+
+   /**/
+   public static LTimerHelpJDialog instance(){
+      if(_instance == null){
+         _instance = new LTimerHelpJDialog(null, "");
+      }
+      return _instance;
+   }
+
+   /**/
+   public void setLocation(int w, int h){
+      Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+      int width  = (int)dim.getWidth();
+      int height = (int)dim.getHeight();
+
+      super.setLocation((width+w+WIDTH)/2, (height-HEIGHT-h)/2);
+   }
+
+   ////////////////////////Private Contructors////////////////////////
+   /**/
+   private LTimerHelpJDialog(JFrame frame, String text){
+      super(frame, "Help", false);
+      if(text != null){
+         this.setText(text);
+      }
+      Container contentPane = this.getContentPane();
+      contentPane.add(this.setNorthPanel(), BorderLayout.NORTH);
+      contentPane.add(this.setCenterPanel(), BorderLayout.CENTER);
+      contentPane.add(this.setButtonPanel(), BorderLayout.SOUTH);
+      this.setSize(WIDTH, HEIGHT);
+      this.setResizable(false);
+   }
+
+   /**/
+   private String helpText(){
+      String ht = new String("LTimer Help Guide\n\n");
+      ht += "This is a typical stop watch, except is in an app\n\n";
+      ht += "To start, press 'Start' or chose 'Start' (ctrl-S) from";
+      ht += " the 'File Menu'\n";
+      ht += "To stop, press 'Stop' or chose 'Stop' (ctrl-T) from ";
+      ht += "the 'File Menu'\n";
+      ht += "To lap, press 'Lap' or chose 'Lap' (ctrl-L) from ";
+      ht += "the 'File Menu'\n";
+      ht += "To reset, press 'Reset' or chose 'Reset' from ";
+      ht += "the 'File Menu'\n";
+      ht += "The User can save the timing data (total elapsed time ";
+      ht += ", current lap time AND all previous Lap Times) ";
+      ht += "via the 'File Menu':  chose 'Save' (ctrl-V)\n\n";
+      return ht;
+   }
+
+   /**/
+   private JPanel setButtonPanel(){
+      JPanel  bPanel = new JPanel();
+      JButton ok     = new JButton("OK");
+      JButton cancel = new JButton("Cancel");
+
+      bPanel.setBorder(BorderFactory.createEmptyBorder(0,15,10,15));
+
+      ok.addActionListener(new ActionListener(){
+         public void actionPerformed(ActionEvent ae){
+            setVisible(false);
+         }
+      });
+
+      cancel.addActionListener(new ActionListener(){
+         public void actionPerformed(ActionEvent ae){
+            setVisible(false);
+          }
+      });
+
+      bPanel.add(ok);
+      bPanel.add(cancel);
+
+      return bPanel;
+   }
+
+   /**/
+   private JPanel setCenterPanel(){
+      JPanel cPanel = new JPanel();
+
+      cPanel.setBorder(BorderFactory.createEmptyBorder(0,15,25,15));
+      cPanel.setLayout(new BorderLayout());
+
+      JTextArea textArea = new JTextArea();
+
+      if(this._helpText == null){
+         textArea.setText(this.helpText());
+      }
+      else{
+         textArea.setText(this._helpText);
+      }
+
+      textArea.setLineWrap(true);
+      textArea.setWrapStyleWord(true);
+      textArea.setEditable(false);
+      textArea.setSelectionStart(0);
+      textArea.setSelectionEnd(0);
+
+      JScrollPane scrollPane = new JScrollPane(textArea);
+      scrollPane.setVerticalScrollBarPolicy(
+                               JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+
+      cPanel.add(scrollPane, BorderLayout.CENTER);
+
+      return cPanel;
+   }
+
+   /**/
+   private JPanel setNorthPanel(){
+      String label  = new String("Simple Help Overview");
+      JPanel nPanel = new JPanel();
+
+      nPanel.setBorder(BorderFactory.createEtchedBorder());
+      nPanel.add(new JLabel(label, SwingConstants.CENTER));
+
+      return nPanel;
+   }
+
+   /**/
+   private void setText(String text){
+      this._helpText = new String(text);
+   }
+}
