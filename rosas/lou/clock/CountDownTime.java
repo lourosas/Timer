ddcbf30671912e84314260c33931267bf7f14e51
Lou Rosas <lourosas@gmail.com>
2021-09-08 16:04:40 -0700

    09-08-21-01

---
+++ rosas/lou/clock/CountDownTime.java
@@ -0,0 +1,110 @@
+//////////////////////////////////////////////////////////////////////
+/*
+* Copyright (C) 2021 Lou Rosas
+* This file is part of many applications registered with
+* the GNU General Public License as published
+* by the Free Software Foundation; either version 3 of the License,
+* or (at your option) any later version.
+* CountdownTimer is distributed in the hope that it will be
+* useful, but WITHOUT ANY WARRANTY; without even the implied
+* warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+* See the GNU General Public License for more details.
+* You should have received a copy of the GNU General Public License
+* along with this program.
+* If not, see <http://www.gnu.org/licenses/>.
+*/
+package rosas.lou.clock;
+
+import java.lang.*;
+import java.util.*;
+import java.io.*;
+import java.time.Instant;
+import java.time.Duration;
+import java.text.ParseException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import rosas.lou.clock.*;
+
+public class CountDownTime{
+   private final int MILLIS = 1000;
+
+   private int    _hours;
+   private int    _minutes;
+   private double _seconds;
+   private double _totalSeconds;
+   private double _totalMilliSeconds;
+   private String _hour;
+   private String _minute;
+   private String _second;
+
+   {
+      _hours             = 0;
+      _minutes           = 0;
+      _seconds           = 0.0;
+      _totalSeconds      = 0.0;
+      _totalMilliSeconds = 0.0;
+      _hour              = null;
+      _minute            = null;
+      _second            = null;
+   };
+
+   //////////////////////////Constructors/////////////////////////////
+   /**/
+   public CountDownTime(String hrs, String mins, String secs) throws
+   NumberFormatException, NullPointerException{
+      try{
+         this._hour    = new String(hrs);
+         this._minute  = new String(mins);
+         this._second  = new String(secs);
+         this.setAllTimes(hrs,mins,secs);
+      }
+      catch(NumberFormatException nfe){ throw nfe; }
+      catch(NullPointerException npe){ throw npe; }
+   }
+
+   /**/
+   public CountDownTime(int hour, int minute, double second){}
+
+   /**/
+   public CountDownTime(double seconds){}
+
+   ///////////////////////Private Methods/////////////////////////////
+   /**/
+   private void calculateTotalSeconds(){
+      final int HOUR_TO_SECONDS = 3600;
+      final int MIN_TO_SECONDS  =   60;
+
+      this._totalSeconds =  this._hours * HOUR_TO_SECONDS;
+      this._totalSeconds += this._minutes * MIN_TO_SECONDS;
+      this._totalSeconds += this._seconds;
+   }
+
+   /**/
+   private void correctHoursandMinutes(){
+      final double SECONDSMOD = 60.;
+      final int MINUTESMOD    =  60;
+      if(this._seconds >= SECONDSMOD){
+      }
+      if(this._minutes >= MINUTESMOD){
+         int mod_minutes = this._minutes % MINUTESMOD;
+         int div_minutes = this._minutes / MINUTESMOD;
+         this._hours  += div_minutes;
+         this._minutes = mod_minutes;
+      }
+   }
+
+   /**/
+   private void setAllTimes(String hrs, String mins, String secs)
+   throws NumberFormatException, NullPointerException{
+      try{
+         this._hours   = Integer.parseInt(hrs);
+         this._minutes = Integer.parseInt(mins);
+         this._seconds = Double.parseDouble(secs);
+         this.correctHoursandMinutes();
+         this.calculateTotalSeconds();
+         this._totalMilliSeconds = this._totalSeconds * MILLIS;
+      }
+      catch(NumberFormatException nfe){ throw nfe; }
+      catch(NullPointerException npe){ throw npe; }
+   }
+}
---
+++ rosas/lou/clock/CountdownTimer.java
@@ -0,0 +1,117 @@
+//////////////////////////////////////////////////////////////////////
+/*
+* Copyright (C) 2021 Lou Rosas
+* This file is part of many applications registered with
+* the GNU General Public License as published
+* by the Free Software Foundation; either version 3 of the License,
+* or (at your option) any later version.
+* LCountdownTimer is distributed in the hope that it will be
+* useful, but WITHOUT ANY WARRANTY; without even the implied
+* warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+* See the GNU General Public License for more details.
+* You should have received a copy of the GNU General Public License
+* along with this program.
+* If not, see <http://www.gnu.org/licenses/>.
+*/
+package rosas.lou.clock;
+
+import java.lang.*;
+import java.util.*;
+import java.io.*;
+import java.time.Instant;
+import java.time.Duration;
+import java.text.ParseException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import rosas.lou.clock.*;
+
+public class CountdownTimer implements ClockObserver{
+   private final int INCREMENT       = 1;
+   private final int MILLISINCREMENT = 1000;
+
+   private CountDownTime _currentTime;
+   private CountDownTime _inputTime;
+   private LClock        _clock;
+   private boolean       _run;
+
+   {
+      _currentTime      = null;
+      _inputTime        = null;
+      _clock            = null;
+      _run              = false;
+   };
+
+   ////////////////////////Constructors///////////////////////////////
+   /**/
+   public CountdownTimer(){}
+
+   /**/
+   public CountdownTimer(LClock clock){
+      this.setClock(clock);
+   }
+
+
+   /////////////////////////Public Methods////////////////////////////
+   /**/
+   public void inputTime(String hrs, String mins, String secs){
+      this._inputTime = new CountDownTime(hrs, mins, secs);
+   }
+
+   /**/
+   public void inputTime(int hour, int minute, double second){
+      this._inputTime = new CountDownTime(hour,minute,second);
+   }
+
+   /**/
+   public void inputTime(double secs){
+      this._inputTime = new CountDownTime(secs);
+   }
+
+   /**/
+   public void setClock(LClock clock){
+      if(this._clock != null){
+         this._clock.removeObserver(this);
+      }
+      this._clock = clock;
+      this._clock.addObserver(this);
+   }
+
+   /**/
+   public void start(){
+      if(this._inputTime != null){
+         this._currentTime = this._inputTime;
+         this.setRun(true);
+      }
+   }
+
+   /**/
+   public void start(String hrs, String mins, String secs){
+      this.inputTime(hrs,mins,secs);
+      this.start();
+   }
+
+   /**/
+   public void start(int hour, int minute, double second){
+      this.inputTime(hour,minute,second);
+      this.start();
+   }
+
+   /**/
+   public void start(double secs){
+      this.inputTime(secs);
+      this.start();
+   }
+
+   ////////////////Clock Observer Implementation methods//////////////
+   /**/
+   public void updateTime(long millis){}
+
+   /**/
+   public void updateTime(Instant instant){}
+
+   ////////////////////////Private Methods////////////////////////////
+   /**/
+   private void setRun(boolean toRun){
+      this._run = toRun;
+   }
+}
