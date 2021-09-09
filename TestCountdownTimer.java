5f292acfe135a385bb438df055df2690a3ce5d69
Lou Rosas <lourosas@gmail.com>
2021-09-09 09:24:08 -0700

    09-09-21-00

---
+++ TestCountdownTimer.java
@@ -0,0 +1,15 @@
+import java.lang.*;
+import java.util.*;
+import rosas.lou.clock.*;
+import myclasses.*;
+
+public class TestCountdownTimer{
+   public static void main(String [] args){
+      new TestCountdownTimer();
+   }
+
+   public TestCountdownTimer(){
+      LClock clock = new LClock();
+      CountdownTimer countdownTimer = new CountdownTimer(clock);
+   }
+}
